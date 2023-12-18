70e27c1f6e7c74d45e9041a489543235f0cb5e14
canaldoovidio <95702586+canaldoovidio@users.noreply.github.com>
2023-12-18 09:42:17 -0300

    arrumando na aula teste

--- app.test.js
+++ app.test.js
@@ -1,10 +1,10 @@
-// const supertest = require('supertest');
-// const app = require('./app');
+const supertest = require('supertest');
+const app = require('./app');
 
-// test('GET /aplicarDesconto/10/5', async () => {
-//     const response = await supertest(app)
-//         .get('/aplicarDesconto/10/5');
+test('GET /aplicarDesconto/10/5', async () => {
+    const response = await supertest(app)
+        .get('/aplicarDesconto/10/5');
  
-//     expect(response.statusCode).toEqual(200);
-//     expect(response.body.valorDescontado).toEqual(5);
-// })
+    expect(response.statusCode).toEqual(200);
+    expect(response.body.valorDescontado).toEqual(5);
+})
